e7400a32df4c798a63fd7ce359ced9addbc25ede
cmgun <cm199311@qq.com>
2023-07-12 11:50:33 +0800

    baidu search head

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -21,6 +21,11 @@ export const Footer = (props) => {
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
                 {BLOG.BEI_AN && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>)}
             </div>
+            <div className='text-center'>
+              <a href="https://hits.seeyoufarm.com">
+                <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fcmgun.cc&count_bg=%2379C83D&title_bg=%23E311A3&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/>
+              </a>
+            </div>
         </div>
     </footer>
 }
